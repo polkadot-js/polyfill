b3cd44f1b2a5e3054384515c005a73b42f1b215b
Jaco Greeff <jacogr@gmail.com>
2020-09-16 19:14:20 +0200

    Direct W3CWebSocket export

--- packages/x-ws/src/websocket.d.ts
+++ packages/x-ws/src/websocket.d.ts
@@ -2,10 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import W3CWebSocket from 'websocket/W3CWebSocket';
-
-const fn = typeof WebSocket === 'undefined'
-  ? W3CWebSocket
-  : WebSocket;
-
-export default fn;
+declare module 'websocket/W3CWebSocket' {
+  export default WebSocket;
+}
