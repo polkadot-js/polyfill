8f9667959c001897aab762f1999a137e789be99f
Jaco Greeff <jacogr@gmail.com>
2020-09-16 23:14:04 +0200

    Simplify detection

--- packages/x-fetch/src/node.ts
+++ packages/x-fetch/src/node.ts
@@ -2,13 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-let fn: typeof fetch;
+import nodeFetch from 'node-fetch';
 
-if (typeof fetch === 'undefined') {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  fn = require('node-fetch');
-} else {
-  fn = fetch;
-}
-
-export default fn;
+export default typeof fetch === 'undefined'
+  ? nodeFetch
+  : fetch;
--- packages/x-ws/src/node.ts
+++ packages/x-ws/src/node.ts
@@ -2,15 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-let fn: typeof WebSocket;
+import { w3cwebsocket } from 'websocket';
 
-if (typeof WebSocket === 'undefined') {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-var-requires
-  const { w3cwebsocket } = require('websocket');
-
-  fn = w3cwebsocket as unknown as typeof WebSocket;
-} else {
-  fn = WebSocket;
-}
-
-export default fn;
+export default typeof WebSocket === 'undefined'
+  ? w3cwebsocket as unknown as typeof WebSocket
+  : WebSocket;
